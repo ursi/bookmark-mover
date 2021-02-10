ed100098ef335818749c990ad2f09ca980c5a00e
Mason Mackaman <masondeanm@aol.com>
2021-02-10 00:29:18 -0500

    Fix new tab issue

--- src/Main.purs
+++ src/Main.purs
@@ -6,9 +6,10 @@ import Chrome.Bookmarks as Bookmarks
 import Chrome.Tabs as Tabs
 import Chrome.WebNavigation (TransitionType(..))
 import Chrome.WebNavigation as WebNav
-import Chrome.Wrap (Chrome)
+import Chrome.Wrap (Chrome, ChromeEvent)
+import Chrome.Wrap as Chrome
 import Data.List ((:))
-import Control.Monad.Except (ExceptT(..), runExceptT)
+import Control.Monad.Except (ExceptT(..), mapExceptT, runExceptT)
 import Control.Monad.Maybe.Trans (MaybeT(..), runMaybeT)
 import Control.Monad.Rec.Class (forever)
 import Control.Monad.State (get, put, runStateT)
@@ -16,9 +17,11 @@ import Control.Monad.Trans.Class (lift)
 import Data.Maybe (isJust)
 import Data.Newtype (unwrap)
 import Debug as Debug
-import Effect.Aff (launchAff_)
+import Effect.Aff (Aff, launchAff_)
+import Effect.Exception (Error)
 import Foreign.Object (Object)
 import Foreign.Object as Obj
+import FRP.Event as Event
 
 bookmarksBarId :: String
 bookmarksBarId = "1"
@@ -38,14 +41,14 @@ main =
                           lift
                             $ Debug.log
                             <$> first
-                                [ Navigated <$> lift2 Tuple WebNav.onBeforeNavigate WebNav.onCommitted
+                                [ Navigated <$> something
                                 -- ^^ don't use parLift2 because the onUpdate listener gets called multiple times ^^ also the comment is down here because purty sux
                                 , BookmarkCreated <$> Bookmarks.onCreated
                                 , BookmarkMoved <$> Bookmarks.onMoved
                                 , BookmarkChanged <$> Bookmarks.onChanged
                                 ]
                         case event of
-                          Navigated ({ url } /\ { transitionType }) -> do
+                          Navigated { url, transitionType } -> do
                             when (transitionType /= Reload)
                               $ ( lift
                                     $ runMaybeT do
@@ -72,7 +75,7 @@ main =
       )
 
 data Event
-  = Navigated (WebNav.BeforeNavigateDetails /\ WebNav.CommittedDetails)
+  = Navigated { url :: String, transitionType :: TransitionType }
   | BookmarkCreated { id :: String, bookmark :: BookmarkTreeNode }
   | BookmarkMoved { id :: String, moveInfo :: Bookmarks.MoveInfo }
   | BookmarkChanged { id :: String, changeInfo :: Bookmarks.ChangeInfo }
@@ -103,3 +106,12 @@ first =
   map runExceptT
     .> parOneOf
     .> ExceptT
+
+something :: Chrome { url :: String, transitionType :: TransitionType }
+something =
+  Event.sampleOn
+    (runExceptT WebNav.onBeforeNavigateE)
+    (runExceptT WebNav.onCommittedE <#> \ocDets obnDets -> lift2 Tuple obnDets ocDets)
+    # ExceptT
+    # Chrome.toChrome
+    <#> \({ url } /\ { transitionType }) -> { url, transitionType }
